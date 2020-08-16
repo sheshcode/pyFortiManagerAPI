766aa41a0b66fb738008b32558b792996bea60d6
akshaymane920 <akshaymane920@gmail.com>
2020-08-16 20:43:33 +0530

    Pulished on pypi.org

---
+++ README.md
@@ -0,0 +1,247 @@
+# pyFortiManagerAPI
+
+A Python wrapper for the FortiManager REST API.
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install pyFortiManagerAPI.
+
+```bash
+pip install pyFortiManager
+```
+
+## Getting Started
+
+1. Creating Instance of the Module
+
+```python
+import pyFortiManager
+fortimngr = pyFortiManagerAPI.FortiManager(host="", username="",password="")
+```
+
+- host: Management Ip address of your FortiManager
+- username/password: Specify your credentials to log into the device.
+
+# User Operations : Address Objects
+
+### 1) Get all address objects from FortiManager.
+
+```python
+>>> fortimngr.get_firewall_address_objects()
+```
+
+### 2) Get specific address object from FortiManager using "name" parameter.
+
+```python
+>>> fortimngr.get_firewall_address_objects(name="YourObjectName")
+```
+
+- ## Parameters
+
+* name: Specify object name that you want to see.
+
+### 3) Create an address object using provided the defined parameters.
+
+```python
+>>> fortimngr.add_firewall_address_object(name="TestObject",
+                                          associated_interface="any",
+                                          subnet=["1.1.1.1", "255.255.255.255"]
+                                          )
+```
+
+- ## Parameters
+
+* name: Specify object name that is to be created
+* associated_interface: Provide interface to which this object belongs if any. {Default is kept any}
+* subnet: Specify the subnet in a list format eg.["1.1.1.1", "255.255.255.255"]
+
+### 4) Update address object.
+
+```python
+>>> fortimngr.update_firewall_address_object(name="TestObject",
+                                             data={"subnet": ["2.2.2.2", "255.255.255.255"]}
+                                             )
+```
+
+- ## Parameters
+
+* name: Enter the name of the object that needs to be updated
+* data: Here the parameter data should be in a Dictonary format. eg. data={"associated_interface": "port1"}
+
+### 5) Delete address object.
+
+```python
+>>> fortimngr.delete_firewall_address_object(object_name="TestObject")
+```
+
+- ## Parameters
+
+* object_name: Specify the Object name you want to delete.
+
+---
+
+# User Operations : Address Groups
+
+### 6) Get all address groups.
+
+```python
+get_address_grps = fortimngr.get_address_group()
+```
+
+### 7) Get specific address group.
+
+```python
+get_address_grps = fortimngr.get_address_group(name="TestGroup")
+```
+
+- ## Parameters
+
+* name: Specify the name the address group.
+
+### 8) Create your own address group.
+
+```python
+>>> fortimngr.add_address_group(name="Test_Group",
+                                members=["TestObject1"])
+```
+
+- ## Parameters
+
+* name: Enter the name of the address group. eg."Test_Group"
+* members: pass your object names as members in a list eg. ["TestObject1", "TestObject2"]
+  > Note: An address group should consist atleast 1 member.
+
+### 9) Update the address group.
+
+```python
+>>> fortimngr.update_address_group(name="Test_Group",
+                                   object_name="TestObject3",
+                                   do="add")
+```
+
+- ## Parameters
+
+* name: Specify the name of the Address group you want to update
+* object_name: Specify name of the object you wish to update(add/remove) in Members List
+* do: Specify if you want to add or remove the object from the members list
+  do="add" will add the object in the address group
+  do="remove" will remove the object from address group
+
+### 10) Delete the address group.
+
+```python
+>>> fortimngr.update_firewall_address_object(name="TestObject",
+                                             data={"subnet": ["2.2.2.2", "255.255.255.255"]}
+                                             )
+```
+
+- ## Parameters
+
+* name: Specify the name of the address you wish to delete
+
+---
+
+# User Operations : Policies
+
+### 11) Get all the policies in your Policy Package.
+
+```python
+>>> fortimngr.update_firewall_address_object(name="TestObject",
+                                             data={"subnet": ["2.2.2.2", "255.255.255.255"]}
+                                             )
+```
+
+- ## Parameters
+
+* policy_package_name: Enter the policy package name.
+* policyid: Can filter and get the policy you want using policyID
+
+### 12) Create your own policy in your Policy Package.
+
+```python
+>>> fortimngr.add_firewall_policy(policy_package_name="YourPolicyPackageName",
+                                  name="YourPolicyName",
+                                  source_interface="port1",
+                                  source_address="all",
+                                  destination_interface="port2",
+                                  destination_address="all",
+                                  service="ALL_TCP",
+                                  logtraffic=2
+                                  )
+
+```
+
+- ## Parameters
+
+* policy_package_name: Enter the name of the policy package eg. "default"
+* name: Enter the policy name in a string format eg. "Test Policy"
+* source_interface: Enter the source interface in a string format eg. "port1"
+* source_address: Enter the src. address object name in string format eg. "LAN_10.1.1.0_24"
+* destination_interface: Enter the source interface in a string format eg. "port2"
+* destination_address: Enter the dst. address object name eg. "WAN_100.25.1.63_32"
+* service: Enter the service you want to permit or deny in string eg. "ALL_UDP"
+* schedule: Schedule time is kept 'always' as default.
+* action: Permit(1) or Deny(0) the traffic. Default is set to Permit.
+* logtraffic: Specify if you need to log all traffic or specific in int format.
+*                  logtraffic=0  Means No Log
+                   logtraffic=1  Means Log Security Events
+                   logtraffic=2  Means Log All Sessions
+
+### 13) Update the policy in your Policy Package.
+
+```python
+>>> fortimngr.update_firewall_policy(policy_package_name="YourPolicyPackageName",
+                                     policyid=10,
+                                     data={
+                                           "srcintf": "port2",
+                                           "action": 0,
+                                           "logtraffic": 0
+                                           })
+```
+
+- ## Parameters
+
+* policy_package_name: Enter the policy package name in which you policy belongs.
+* policyid: Enter the Policy ID you want to edit
+* data: Enter the updated data in a dictionary format eg. {"srcaddr":"LAN_10.1.1.0_24"}
+
+### 14) Delete the policy in your Policy Package.
+
+```python
+>>> fortimngr.delete_firewall_policy(policy_package_name="YourPolicyPackageName",
+                                     policyid=10)
+```
+
+- ## Parameters
+
+* policy_package_name: Enter the policy package name in which you policy belongs
+* policyid: Enter the policy ID of the policy you want to delete
+
+---
+
+# User Operations : Installing the Policy Package.
+
+### 15) Installing the Policy Package.
+
+```python
+>>> fortimngr.install_policy_package(package_name="Your Policy Package name")
+
+```
+
+- ## Parameters
+
+* package_name: Enter the package name you wish to install
+
+---
+
+## Future Tasks
+
+- This module is tested on Fortimanager v6.2.2 on "root" adom. It still does support multiple Adoms. So I will try to get this working for Multiple adoms too.
+
+## Contributing
+
+- Being new to Python and this being my first publish, to get this module fully working for all of us the Pull requests are welcome.
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ src/pyFortiManagerAPI.py
@@ -0,0 +1,427 @@
+__author__ = "Akshay Mane"
+
+import requests
+import urllib3
+
+# Disable for insecure connections warnings
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+class FortiManager:
+    def __init__(self, host, username, password, vdom="root", verify=False):
+        protocol = "https"
+        self.host = host
+        self.username = username
+        self.password = password
+        self.vdom = vdom
+        self.sessionid = "null"
+        self.verify = verify
+        if not self.verify:
+            protocol = "http"
+        self.base_url = f"{protocol}://{host}/jsonrpc"
+
+    # Login Method
+    def login(self):
+        """
+        Log in to FortiManager with the details provided during object creation of this class
+        :return: Session
+        """
+        session = requests.session()
+        payload = \
+            {
+                "method": "exec",
+                "params":
+                    [
+                        {
+                            "data": {
+                                "passwd": self.password,
+                                "user": self.username
+                            },
+                            "url": "sys/login/user"
+                        }
+                    ],
+                "session": self.sessionid
+            }
+        payload = repr(payload)
+        login = session.post(url=self.base_url, data=payload, verify=self.verify)
+        self.sessionid = login.json()['session']
+        return session
+
+    def logout(self):
+        """
+        Logout from FortiManager
+        :return: Response of status code with data in JSON Format
+        """
+        session = requests.session()
+        payload = \
+            {
+                "method": "exec",
+                "params":
+                    [
+                        {
+                            "url": "sys/logout"
+                        }
+                    ],
+                "session": self.sessionid
+            }
+        payload = repr(payload)
+        logout = session.post(url=self.base_url, data=payload, verify=self.verify)
+        return logout.json()["result"]
+
+    # Firewall Object Methods
+    def get_firewall_address_objects(self, name=False):
+        """
+        Get all the address objects data stored in FortiManager
+        :return: Response of status code with data in JSON Format
+        """
+        url = f"pm/config/adom/root/obj/firewall/address"
+        if name:
+            url = f"pm/config/adom/root/obj/firewall/address/{name}"
+        session = self.login()
+        payload = \
+            {
+                "method": "get",
+                "params": [
+                    {
+                        "url": url
+                    }
+                ],
+                "session": self.sessionid
+            }
+        payload = repr(payload)
+        get_address_objects = session.post(url=self.base_url, data=payload, verify=self.verify)
+        return get_address_objects.json()["result"]
+
+    def add_firewall_address_object(self, name, associated_interface="any", subnet=list, object_type=0,
+                                    allow_routing=0):
+        """
+        Create an address object using provided info
+        :param name: Enter object name that is to be created
+        :param associated_interface: Provide interface to which this object belongs if any. {Default is kept any}
+        :param subnet: Enter the subnet in a list format eg.["1.1.1.1", "255.255.255.255"]
+        :param object_type:
+        :param allow_routing: Set routing if needed
+        :return: Response of status code with data in JSON Format
+        """
+        session = self.login()
+        payload = {
+            "method": "add",
+            "params": [
+                {
+                    "data": {
+                        "allow-routing": allow_routing,
+                        "associated-interface": associated_interface,
+                        "name": name,
+                        "subnet": subnet,
+                        "type": object_type
+                    },
+                    "url": f"pm/config/adom/root/obj/firewall/address"
+                }
+            ],
+            "session": self.sessionid
+        }
+        payload = repr(payload)
+        add_address_objects = session.post(url=self.base_url, data=payload, verify=self.verify)
+        return add_address_objects.json()["result"]
+
+    def update_firewall_address_object(self, name, data=dict):
+        """
+        Get the name of the address object and update it with your data
+
+        :param name: Enter the name of the object that needs to be updated
+        :param data: Enter the data in a dictionary
+        :return: Response of status code with data in JSON Format
+        """
+        session = self.login()
+        data.update({"name": name})
+        payload = \
+            {
+                "method": "update",
+                "params": [
+                    {
+                        "data": data,
+                        "url": f"pm/config/adom/root/obj/firewall/address/"
+                    }
+                ],
+                "session": "None"
+            }
+        payload = repr(payload)
+        update_firewall_object = session.post(url=self.base_url, data=payload, verify=self.verify)
+        return update_firewall_object.json()["result"]
+
+    def delete_firewall_address_object(self, object_name):
+        """
+        Delete the address object if no longer needed using object name
+
+        :param object_name: Enter the Object name you want to delete
+        :return: Response of status code with data in JSON Format
+        """
+        session = self.login()
+        payload = \
+            {
+                "method": "delete",
+                "params": [
+                    {
+                        "url": f"pm/config/adom/root/obj/firewall/address/{object_name}"
+                    }
+                ],
+                "session": self.sessionid
+            }
+        payload = repr(payload)
+        delete_address_object = session.post(url=self.base_url, data=payload, verify=self.verify)
+        return delete_address_object.json()["result"]
+
+    # Firewall Address Groups Methods
+    def get_address_group(self, name=False):
+        """
+        Get the address groups created in your FortiManager
+        :param name: You can filter out the specific address group which you want to see
+        :return: Response of status code with data in JSON Format
+        """
+        session = self.login()
+        url = "pm/config/adom/root/obj/firewall/addrgrp"
+        if name:
+            url = f"pm/config/adom/root/obj/firewall/addrgrp/{name}"
+        payload = \
+            {
+                "method": "get",
+                "params": [
+                    {
+                        "url": url
+                    }
+                ],
+                "session": self.sessionid
+            }
+        payload = repr(payload)
+        get_address_group = session.post(url=self.base_url, data=payload, verify=self.verify)
+        return get_address_group.json()["result"]
+
+    def add_address_group(self, name, members=list):
+        """
+        Create your own group with just 2 parameters
+        :param name: Enter the name of the address group                eg."Test_Group"
+        :param members: pass your object names as members in a list     eg. ["LAN_10.1.1.0_24, "INTERNET"]
+        :return: Response of status code with data in JSON Format
+        """
+        session = self.login()
+        payload = \
+            {
+                "method": "add",
+                "params": [
+                    {
+                        "data": {
+                            "name": name,
+                            "member": members,
+                        },
+                        "url": "pm/config/adom/root/obj/firewall/addrgrp"
+                    }
+                ],
+                "session": self.sessionid
+            }
+        payload = repr(payload)
+        add_address_group = session.post(url=self.base_url, data=payload, verify=self.verify)
+        return add_address_group.json()["result"]
+
+    def update_address_group(self, name, object_name, do="add"):
+        """
+        Update Members of the Address group
+        :param name: Specify the name of the Address group you want to update
+        :param object_name: Specify name of the object you wish to update(add/remove) in Members List
+        :param do: Specify if you want to add or remove the object from the members list
+                    do="add"    will add the object in the address group
+                    do="remove" will remove the object from address group
+        :return: Response of status code with data in JSON Format
+        """
+        session = self.login()
+        get_addr_group = self.get_address_group(name=name)
+        members = get_addr_group[0]['data']['member']
+        if do == "add":
+            members.append(object_name)
+        elif do == "remove":
+            members.remove(object_name)
+
+        payload = \
+            {
+                "method": "update",
+                "params": [
+                    {
+                        "data": {
+                            "member": members,
+                        },
+                        "url": f"pm/config/adom/root/obj/firewall/addrgrp/{name}"
+                    }
+                ],
+                "session": self.sessionid
+            }
+        payload = repr(payload)
+        update_address_group = session.post(url=self.base_url, data=payload, verify=False)
+        return update_address_group.json()["result"]
+
+    def delete_address_group(self, name):
+        """
+        Delete the Address group if no longer needed
+        :param name: Specify the name of the address you wish to delete
+        :return: Response of status code with data in JSON Format
+        """
+        session = self.login()
+        payload = \
+            {
+                "method": "delete",
+                "params": [
+                    {
+                        "data": {
+                        },
+                        "url": f"pm/config/adom/root/obj/firewall/addrgrp/{name}"
+                    }
+                ],
+                "session": self.sessionid
+            }
+        payload = repr(payload)
+        delete_address_group = session.post(url=self.base_url, data=payload, verify=False)
+        return delete_address_group.json()["result"]
+
+    # Firewall Policies Methods
+    def get_firewall_policies(self, policy_package_name="default", policyid=False):
+        """
+        Get the firewall policies present in the policy package
+
+        :param policy_package_name: Enter the policy package name
+        :param policyid: Can filter and get the policy you want using policyID
+        :return: Response of status code with data in JSON Format
+        """
+        url = f"pm/config/adom/root/pkg/{policy_package_name}/firewall/policy/"
+        if policyid:
+            url = url + str(policyid)
+        session = self.login()
+        payload = {
+            "method": "get",
+            "params": [
+                {
+                    "url": url
+                }
+            ],
+            "session": self.sessionid
+        }
+        payload = repr(payload)
+        get_firewall_policies = session.post(url=self.base_url, data=payload, verify=self.verify)
+        return get_firewall_policies.json()["result"]
+
+    def add_firewall_policy(self, policy_package_name="default", name=str, source_interface=str, source_address=str,
+                            destination_interface=str, destination_address=str, service=str, schedule="always",
+                            action=1, logtraffic=int, ):
+        """
+        Create your own policy in FortiManager using the instance parameters.
+
+        :param policy_package_name: Enter the name of the policy package                eg. "default"
+        :param name: Enter the policy name in a string format                           eg. "Test Policy"
+        :param source_interface: Enter the source interface in a string format          eg. "port1"
+        :param source_address: Enter the src. address object name in string format      eg. "LAN_10.1.1.0_24"
+        :param destination_interface: Enter the source interface in a string format     eg. "port2"
+        :param destination_address: Enter the dst. address object name                  eg. "WAN_100.25.1.63_32"
+        :param service: Enter the service you want to permit or deny in string          eg. "ALL_TCP"
+        :param schedule: Schedule time is kept 'always' as default.
+        :param action: Permit(1) or Deny(0) the traffic. Default is set to Permit.
+        :param logtraffic: Specify if you need to log all traffic or specific in int format.
+                            logtraffic=0: Means No Log
+                            logtraffic=1 Means Log Security Events
+                            logtraffic=2 Means Log All Sessions
+        :return: Response of status code with data in JSON Format
+        """
+        session = self.login()
+        payload = {
+            "method": "add",
+            "params": [
+                {
+                    "data": {
+                        "dstaddr": destination_address,
+                        "dstintf": destination_interface,
+                        "logtraffic": logtraffic,
+                        "name": name,
+                        "schedule": schedule,
+                        "service": service,
+                        "srcaddr": source_address,
+                        "srcintf": source_interface,
+                        "action": action
+                    },
+                    "url": f"pm/config/adom/root/pkg/{policy_package_name}/firewall/policy/"
+                }
+            ],
+            "session": self.sessionid
+        }
+        payload = repr(payload)
+        add_policy = session.post(url=self.base_url, data=payload, verify=self.verify)
+        return add_policy.json()["result"]
+
+    def update_firewall_policy(self, policy_package_name, policyid, data):
+        """
+        Update your policy with your specific needs
+
+        :param policy_package_name: Enter the policy package name in which you policy belongs
+        :param policyid: Enter the Policy ID you want to edit
+        :param data: Enter the updated data in a dictionary format eg. {"srcaddr":"LAN_10.1.1.0_24"}
+        :return: Response of status code with data in JSON Format
+        """
+        session = self.login()
+        payload = \
+            {
+                "method": "update",
+                "params": [
+                    {
+                        "data": data,
+                        "url": f"pm/config/adom/root/pkg/{policy_package_name}/firewall/policy/{policyid}"
+                    }
+                ],
+                "session": self.sessionid
+            }
+        payload = repr(payload)
+        update_policy = session.post(url=self.base_url, data=payload, verify=self.verify)
+        return update_policy.json()["result"]
+
+    def delete_firewall_policy(self, policy_package_name, policyid):
+        """
+        Delete the policy if not is use with the policyID
+
+        :param policy_package_name: Enter the policy package name in which you policy belongs
+        :param policyid: Enter the policy ID of the policy you want to delete
+        :return: Response of status code with data in JSON Format
+        """
+        session = self.login()
+        payload = \
+            {
+                "method": "delete",
+                "params": [
+                    {
+                        "url": f"pm/config/adom/root/pkg/{policy_package_name}/firewall/policy/{policyid}"
+                    }
+                ],
+                "session": self.sessionid
+            }
+        payload = repr(payload)
+        delete_policy = session.post(url=self.base_url, data=payload, verify=self.verify)
+        return delete_policy.json()["result"]
+
+    def install_policy_package(self, package_name):
+        """
+        Install the policy package on your Fortigate Firewalls
+
+        :param package_name: Enter the package name you wish to install
+        :return: Response of status code with data in JSON Format
+        """
+        session = self.login()
+        payload = \
+            {
+                "method": "exec",
+                "params": [
+                    {
+                        "data": {
+                            "adom": "root",
+                            "pkg": f"{package_name}"
+                        },
+                        "url": "securityconsole/install/package"
+                    }
+                ],
+                "session": self.sessionid
+            }
+        payload = repr(payload)
+        install_package = session.post(url=self.base_url, data=payload, verify=self.verify)
+        return install_package.json()["result"]
