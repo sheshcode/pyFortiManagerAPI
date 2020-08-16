0896af96489cdc86e7db5256bd09941f61217c91
akshaymane920 <akshaymane920@gmail.com>
2020-08-16 22:11:48 +0530

    corrected docs

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setup(
     name='pyFortiManagerAPI',
     description='A Python wrapper for the FortiManager REST API',
-    version='0.0.2',
+    version='0.0.4',
     py_modules=["pyFortiManagerAPI"],
     package_dir={'': 'src'},
     classifiers=[
